c6c127740b6d8d106e13cce3152fe49c4037f5a8
sisfs <m.tallant+github@gmail.com>
2016-11-23 19:48:04 -0500

    adding an installer for freebsd

---
+++ freebsdInstall.sh
@@ -0,0 +1,6 @@
+
+pkg install pv lzop mbuffer p5-Config-Inifiles nano cmdwatch 
+ln -s /usr/bin/lzop /usr/local/bin/lzop
+sudo ln -s ~/sanoid/ /etc/sanoid
+sudo ln -s /etc/sanoid/sanoid /usr/local/bin/sanoid
+sudo ln -s /etc/sanoid/syncoid /usr/local/bin/syncoid
