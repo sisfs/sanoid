8e4f673d70df6dc57e85e558b57fff133486eb1a
sisfs <m.tallant+github@gmail.com>
2016-12-18 10:46:05 -0500

    freebsdInstall.sh edits
    added a link for perl
    reversed the link for lzop (oops!)
    need to add onscreen instructions for crontab still

--- freebsdInstall.sh
+++ freebsdInstall.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 
-pkg install pv lzop mbuffer p5-Config-Inifiles nano cmdwatch 
-ln -s /usr/bin/lzop /usr/local/bin/lzop
+sudo pkg install pv lzop mbuffer p5-Config-Inifiles nano cmdwatch 
+sudo ln -s /usr/local/bin/lzop /usr/bin/lzop
+ln -s /usr/local/bin/perl /usr/bin/perl
 sudo ln -s ~/sanoid/ /etc/sanoid
 sudo ln -s /etc/sanoid/sanoid /usr/local/bin/sanoid
 sudo ln -s /etc/sanoid/syncoid /usr/local/bin/syncoid
