fa06986e8c77e92a3b49fc6262e143c7e065c784
sisfs <m.tallant+github@gmail.com>
2017-07-28 11:30:54 -0400

    updated the freebsdinstaller script to include the new symlinks
    the merge commit required that i make a choice to change syncoid's calls to pv, mbuffer and sudo or, i could symlink them to the expected location...
    the latter choice made more sense.

--- freebsdInstall.sh
+++ freebsdInstall.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-sudo pkg install pv lzop mbuffer p5-Config-Inifiles nano cmdwatch 
+# install the necessary packages. 
+# IMHO pv and mbuffer work fine of FreeBSD
+sudo pkg install pv lzop mbuffer p5-Config-Inifiles
+
+# install the symlinks required to make this look like a Linux system =)
+sudo ln -s /usr/local/bin/mbuffer /usr/bin/mbuffer
+sudo ln -s /usr/local/bin/sudo /usr/bin/sudo
+sudo ln -s /usr/local/bin/pv /usr/bin/pv
 sudo ln -s /usr/local/bin/lzop /usr/bin/lzop
-ln -s /usr/local/bin/perl /usr/bin/perl
+sudo ln -s /usr/local/bin/perl /usr/bin/perl
+
+# install the symlinks required for sanoid to work from the command line without absolute paths
 sudo ln -s ~/sanoid/ /etc/sanoid
 sudo ln -s /etc/sanoid/sanoid /usr/local/bin/sanoid
 sudo ln -s /etc/sanoid/syncoid /usr/local/bin/syncoid
+sudo ln -s /etc/sanoid/findoid /usr/local/bin/findoid
